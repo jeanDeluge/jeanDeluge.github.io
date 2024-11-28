c4688ac3e6148056bc5e50dbacd3866c3cedfa5a
jeanDeluge <jeandelugedev@gmail.com>
2024-11-28 10:37:23 +0900

    Q17: 경쟁적 전염
    해답보고 풀었음.

--- categories/CodingTest/advanced/once/dfs_bfs/Q17.py
+++ categories/CodingTest/advanced/once/dfs_bfs/Q17.py
@@ -18,31 +18,46 @@ dy = [ 1, -1, 0, 0]
 def solution(input):
     graph_size, virus = list(map(int, input[0].split()))
     graph = [list(map(int, i.split())) for i in input[1]]
-    after, get_x, get_y = map(int, input[2].split())
+    time, target_x, target_y = map(int, input[2].split())
     
-    virus_number = deque([i for i in range(1, virus+1)])
-
-    def bfs():
-        current_virus = virus_number.popleft()
-        for i in range(graph_size):
-            for j in range(graph_size):
-                if graph[i][j] == current_virus:
-                    curr_x = i
-                    curr_y = j
-                    
-                    for i in range(4):
-                        nx = curr_x + dx[i]
-                        ny = curr_y + dy[i]
-                        
-                        if nx > 0 and nx < graph_size and ny > 0 and ny < graph_size and graph[nx][ny] == 0:
-                            graph[nx][ny] = current_virus
-        virus_number.append(current_virus)
+    data = []
+    
+    for i in range(graph_size):
+        for j in range(graph_size):
+            if graph[i][j] != 0:
+                data.append((graph[i][j],0, i, j)) # 바이러스 종류, 시간, 위치 x, y
+                
+    data.sort() #낮은번호부터 정렬
+    
+    q = deque(data)
+    
+    while q:
+        virus, s, x, y = q.popleft()
+        if s == time:
+            break
         
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if 0 <= nx and nx < graph_size and 0 <= ny and ny < graph_size:
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = virus
+                    q.append((virus, s+1, nx, ny))
 
-    for i in range(after):
-        bfs()
-    print(graph)
-    print()
+    print(graph[target_x-1][target_y-1])
+        
+    
+    
+                
+
+        
+        
+        
+        
+        
+    
+    
 
 solution(input_1)
 solution(input_2)
